1b213e26089f185675c6dd6bf956f80ec88e50aa
Giampaolo Rodola <g.rodola@gmail.com>
2012-01-12 18:42:17 +0000

    tagging old releases taken from pypi

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+from distutils.core import Extension
+
+module1 = Extension('sendfile',
+                    sources = ['sendfilemodule.c'])
+
+setup (name = 'py-sendfile',
+       version = '1.2.2',
+       description = 'A Python interface to sendfile(2)',
+       author='Ben Woolley', author_email='user ben at host tautology.org',
+       ext_modules = [module1])
