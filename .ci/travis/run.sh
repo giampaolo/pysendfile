e9f361b0f9b2cb677c58681d731626f2e729bcaa
Giampaolo Rodola <g.rodola@gmail.com>
2016-02-25 22:45:05 +0100

    ad OSX travis integration

---
+++ .ci/travis/run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+set -x
+
+if [[ "$(uname -s)" == 'Darwin' ]]; then
+    if which pyenv > /dev/null; then
+        eval "$(pyenv init -)"
+    fi
+    pyenv activate psutil
+fi
+
+python setup.py build
+python setup.py develop
+python test/test_sendfile.py
+flake8
